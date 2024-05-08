02ef65eb591c32eb9e72f48f991fcbc119f54a3e
Volodymyr Khymych <51357944+vovik541@users.noreply.github.com>
2024-05-07 22:38:39 -0700

    Task 1 done

---
+++ src/main/java/lab1/task4/BallThread.java
@@ -0,0 +1,24 @@
+package lab1.task4;
+
+public class BallThread extends Thread {
+    private Ball b;
+
+    public BallThread(Ball ball) {
+        b = ball;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i < 10000; i++) {
+                b.move();
+                System.out.println("Thread name = "
+                        + Thread.currentThread().getName());
+                Thread.sleep(5);
+
+            }
+        } catch (InterruptedException ex) {
+
+        }
+    }
+}
